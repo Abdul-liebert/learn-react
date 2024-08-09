a8e919dd53954d00d2e5a429f8e4b6045b9f061e
fadhil1806 <fadhil8637@smk.belajar.id>
2024-08-09 08:17:08 +0700

    fix

--- todo-react/src/index.js
+++ todo-react/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TodoApp from './todoApp/todoApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    React.StrictMode >
-    <
-    TodoApp / >
-    <
-    /React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
